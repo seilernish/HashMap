ce7f5cb25382f0f92d3fa9639de9f32e9bc8e93c
mr-kiesel <thomas.kiesel@fwisd.org>
2018-02-14 09:12:04 -0600

    Create PaschalHashMap.java

---
+++ PaschalHashMap.java
@@ -0,0 +1,56 @@
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class PaschalHashMap<K,V> {
+
+	public PaschalHashMap()
+	{
+		
+	}
+	
+	public int size()
+	{
+		return -1;
+	}
+	
+	public boolean isEmpty()
+	{
+		return false;
+	}
+	
+	public V get(K key)
+	{
+		return null;
+	}
+	
+	public V remove(K key)
+	{
+		return null;
+	}
+	
+	public void put(K key, V value)
+	{
+		
+	}
+	
+	public void clear()
+	{
+		
+	}
+	
+	public boolean containsKey(K key)
+	{
+		return false;
+	}
+	
+	public boolean containsValue(V value)
+	{
+		return false;
+	}
+	
+	public Collection<V> values()
+	{
+		return null;
+	}
+	
+}
